8f5b5c7429cdc8540170580a34f2213fdde962f4
SaschaPro <s.proschinger@outlook.com>
2023-10-15 11:28:28 +0200

    Added support for declination consideration

--- source/controllers/sunPosition.ts
+++ source/controllers/sunPosition.ts
@@ -1,6 +1,8 @@
 import { formatResponse, isInRange, isNumeric } from "../utils";
+import GeoMag from 'geomag';
 
 const SunCalc = require('suncalc3');
+
 const latitudeFallback = 48.6586252;
 const longitudeFallback = 16.2444244;
 const minAltitudeDefault = 0;
@@ -9,23 +11,27 @@ const maxAltitudeDefault = 90;
 export function isExposed(query: any) {
     console.log(`Sun exposure requested for query ${JSON.stringify(query)}`);
 
-    const latitude = getValidValue(query.latitude, -90, 90, latitudeFallback, "Latitude");
-    const longitude = getValidValue(query.longitude, -180, 180, longitudeFallback, "Longitude");
+    const latitude = getValidValue(query.latitude, -90, 90, latitudeFallback);
+    const longitude = getValidValue(query.longitude, -180, 180, longitudeFallback);
     const minAltitude = query.minAltitude ? query.minAltitude : minAltitudeDefault;
     const maxAltitude = query.maxAltitude ? query.maxAltitude : maxAltitudeDefault;
+    const correctDeclination = query.correctDeclination ? query.correctDeclination.toLowerCase() === 'true' : true;
+    const minAzimuth = correctDeclination ? correctAngle(query.minAzimuth, latitude, longitude) : query.minAzimuth;
+    const maxAzimuth = correctDeclination ? correctAngle(query.maxAzimuth, latitude, longitude) : query.maxAzimuth;
 
     const params = JSON.stringify({
         latitude: latitude,
         longitude: longitude,
-        minAzimuth: query.minAzimuth,
-        maxAzimuth: query.maxAzimuth,
+        minAzimuth: minAzimuth,
+        maxAzimuth: maxAzimuth,
         minAltitude: minAltitude,
         maxAltitude: maxAltitude,
     });
     console.log(`Using ${params} for calculation`);
 
     const currentSunPosition = SunCalc.getPosition(new Date(), latitude, longitude);
-    const azimuthExposed = isAngleInRange(currentSunPosition.azimuthDegrees, query.minAzimuth, query.maxAzimuth);
+
+    const azimuthExposed = isAngleInRange(currentSunPosition.azimuthDegrees, minAzimuth, maxAzimuth);
     const altitudeExposed = isAngleInRange(currentSunPosition.altitudeDegrees, minAltitude, maxAltitude);
 
     const response = formatResponse(azimuthExposed && altitudeExposed);
@@ -35,7 +41,7 @@ export function isExposed(query: any) {
     return response;
 }
 
-function getValidValue(value: any, min: number, max: number, fallback: number, name: string): number {
+function getValidValue(value: any, min: number, max: number, fallback: number): number {
     if (isNumeric(value) && isInRange(value, min, max)) {
         return value;
     } else {
@@ -49,4 +55,10 @@ function isAngleInRange(value: number, min: number, max: number): boolean {
     } else {
         return value >= min || value <= max;
     }
+}
+
+function correctAngle(value: number, latitude: number, longitude: number): number {
+    const currentDeclination: number = GeoMag.field(latitude, longitude).declination;
+    console.log(`Calculated declination: ${currentDeclination}`);
+    return +value + +currentDeclination;
 }
